646a6d783bc4088323f085935c25aaad0842fcaa
KSI <ksi52875138@gmail.com>
2025-05-20 21:19:46 +0800

    調整Controller、增加IsCompelet狀態

--- src/main/java/com/example/TodoList/Controller/TodoController.java
+++ src/main/java/com/example/TodoList/Controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.TodoList.Controller;
 
 import com.example.TodoList.Repository.TodoRepository;
 import com.example.TodoList.Entity.Todo;
+import com.example.TodoList.Service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,26 +13,30 @@ import java.util.List;
 @CrossOrigin(origins = "*") //允許前端跨域存取
 public class TodoController {
     @Autowired
-    private TodoRepository repo;
+    private TodoService service;
 
     @GetMapping
-    public List<Todo> getAll(){
-        return repo.findAll();
+    public List<Todo> getAll() {
+        return service.getAll();
     }
 
     @PostMapping
-    public Todo create(@RequestBody Todo todo){
-        return repo.save(todo);
+    public Todo create(@RequestBody Todo todo) {
+        return service.create(todo);
     }
 
     @PutMapping("/{id}")
-    public Todo update(@PathVariable Long id, @RequestBody Todo todo){
-        todo.setId(id);
-        return repo.save(todo);
+    public Todo update(@PathVariable Long id, @RequestBody Todo todo) {
+        return service.update(id, todo);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        repo.deleteById(id);
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+    @PatchMapping
+    public Todo toggleComplete(@PathVariable Long id) {
+        return service.toggleComplete(id);
     }
 }
--- src/main/java/com/example/TodoList/Entity/Todo.java
+++ src/main/java/com/example/TodoList/Entity/Todo.java
@@ -11,13 +11,17 @@ public class Todo {
     private Long id;
 
     private String title;
-    private boolean completed;
+    private boolean completed = false;
 
-//    public Long getId(){
-//        return id
-//    }
-//
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setIsCompleted(boolean b) {
+    }
+
+    public boolean getIsCompleted() {
+        return false;
     }
 }
